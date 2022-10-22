f6b938d0ea22111827501d36484fa92a8e588124
Itamora Cyrew <109291902+itamora@users.noreply.github.com>
2022-10-22 22:21:01 +0800

    test-6

--- lib.js
+++ lib.js
@@ -2,18 +2,36 @@ function range(start, end, step) {
 	if (start < 0) {
 		start = 0;
 	};
-	let array = [];
+	var array = [];
 	for (let i = start; i < end; i += step) {
 		array.push(i);
 	};
 	return array;
 };
 
+function ToHex(rgba) {
+	var hex = rgba.split(/, /);
+	for (let i = 0; i < 4; i++) {
+		switch (i) {
+			case 3:
+				hex[i] = hex[i] * 255;
+				break;
+		};
+		hex[i] = parseInt(hex[i]).toString(16);
+		switch (hex[i].length) {
+			case 1:
+				hex[i] = 0 + hex[i];
+				break;
+		};
+	};
+	return "#" + hex.join("");
+};
+
 function calcDay(date, month, fullYear) {
-	let y = parseInt(`${fullYear}`.substring(`${fullYear}`.length - 2));
-	let c = parseInt(`${fullYear}`.substring(0, `${fullYear}`.length - 2));
-	let m = month;
-	let d = date;
+	var y = parseInt(`${fullYear}`.substring(`${fullYear}`.length - 2));
+	var c = parseInt(`${fullYear}`.substring(0, `${fullYear}`.length - 2));
+	var m = month;
+	var d = date;
 	switch (month) {
 		case 0:
 			m = 13;
